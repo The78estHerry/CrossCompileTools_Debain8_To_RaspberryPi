91e3b376c45e15978adb1d389054c4dba5a5783a
root <root@debian64-vaio.littleherry.net>
2017-02-27 14:37:37 +0800

    added other...

---
+++ lib/gcc/arm-linux-gnueabihf/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --prefix=/opt/cross --target=arm-linux-gnueabihf --enable-languages=c,c++ --with-float=hard --disable-werror --with-sysroot=/home/pi/RPI_ROOT --with-build-sysroot=/home/pi/RPI_ROOT";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "float", "hard" }, { "tls", "gnu" } };
